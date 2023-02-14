36c191c16334db3e24e8bf36f5c7099a64659c4c
GIN <740415505@qq.com>
2023-02-14 20:22:10 +0800

    net core实现二维码
    QRCode不支持.net core6，项目用的是net core5

---
+++ QRCodeCore/QRCodeCore/Controllers/QRCodeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QRCodeCore.Models;
+using QRCoder;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using static QRCoder.PayloadGenerator;
+
+namespace QRCodeCore.Controllers
+{
+    public class QRCodeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(QRCodeModel model)
+        {
+            Payload payload = null;
+            switch (model.QRCodeType)
+            {
+                case "web":
+                    payload = new Url(model.WebSiteURL);
+                    break;
+                case "bookmark":
+                    payload = new Bookmark(model.BookMarkURL, model.BookMarkURL);
+                    break;
+                case "sms":
+                    payload = new SMS(model.SMSPhoneNumber, model.SMSBody);
+                    break;
+                case "email":
+                    payload = new Mail(model.ReceiverEmailAddress, model.EmailSubject, model.EmailMessage);
+                    break;
+                case "wifi":
+                    payload = new WiFi(model.WIFIName, model.WIFIPassWord, WiFi.Authentication.WPA);
+                    break;
+            }
+
+            QRCodeGenerator generator = new QRCodeGenerator();
+            QRCodeData qrCodeData = generator.CreateQrCode(payload);
+            QRCode qrCode = new QRCode(qrCodeData);
+            var qrCodeAsBitmap = qrCode.GetGraphic(20);
+
+            string base64String = Convert.ToBase64String(BitmapToByteArray(qrCodeAsBitmap));
+            model.QRImageURL = "data:image/png;base64," + base64String;
+            return View("Index", model);
+        }
+
+        private byte[] BitmapToByteArray(Bitmap bitmap)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bitmap.Save(ms, ImageFormat.Png);
+                return ms.ToArray();
+            }
+        }
+    }
+}
---
+++ QRCodeCore/QRCodeCore/Models/QRCodeModel.cs
@@ -0,0 +1,29 @@
+﻿namespace QRCodeCore.Models
+{
+    public class QRCodeModel
+    {
+        public string QRCodeType { get; set; }
+
+        public string QRImageURL { get; set; }
+
+        public string BookMarkTitle { get; set; }
+
+        public string BookMarkURL { get; set; }
+
+        public string ReceiverEmailAddress { get; set; }
+
+        public string EmailSubject { get; set; }
+
+        public string EmailMessage { get; set; }
+
+        public string SMSPhoneNumber { get; set; }
+
+        public string SMSBody { get; set; }
+
+        public string WebSiteURL { get; set; }
+
+        public string WIFIName { get; set; }
+
+        public string WIFIPassWord { get; set; }
+    }
+}
